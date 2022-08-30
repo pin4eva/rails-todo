5420908678ee4a52b25c236d9777a3c665f567af
Peter Akaliro <pin4eva@hotmail.com>
2022-08-30 08:48:00 +0100

    created tiny project

---
+++ config/initializers/redis.rb
@@ -0,0 +1,1 @@
+# Redis.current = Redis.new(url: "redis://127.0.0.1", port: 6379, db: "todo")
---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  devise_for :users
+  as :user do
+    get "signin" => "devise/sessions#new"
+    delete "signout" => "devise/sessions#destroy"
+    get "signup" => "devise/registrations#new"
+  end
+  root "pages#home"
+  get "about" => "pages#about"
+  get "contact" => "pages#contact-us"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
